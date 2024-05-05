9fe37e968a660240fc06724359159450594f2de3
jeremymiru <mirujeremy@gmail.com>
2024-05-05 10:19:25 +0300

    last commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
 import React from "react";
-import blogData from "../data/blog";
-
-console.log(blogData);
+import About from "./About"
+import ArticleList from "./ArticleList";
+import Header from "./Header";
+import logo from "../assets/logo";
 
 function App() {
+  let articles = [
+    {
+      
+      title: "Article 1",
+      date: "December 15, 2020",
+      preview: "This is my preview for article 1",
+      
+    },
+    {
+
+      title: "Article 2",
+      date: "December 11, 2020",
+      preview: "This is my preview for article 2",
+
+    
+    },
+    {
+      title: "Article 3",
+      date: "December 10, 2020",
+      preview: "This is my preview for article 3",
+    }
+  ];
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <div>
+      <Header name="Projects Based Blog" />
+        <About
+          image="https://example.com/my-image.jpg"
+          aboutText="This is some information about my blog."
+        />
+        <ArticleList articles={articles} />
+      </div>
+
     </div>
   );
 }
